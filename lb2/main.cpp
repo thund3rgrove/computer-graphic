4ba46ed022bd80008dc88a3983822c98c8598869
thund3rgrove <alex007613@gmail.com>
2024-03-22 00:27:42 +0800

    quickfix

--- lb2/main.cpp
+++ lb2/main.cpp
@@ -18,6 +18,7 @@ void EnableOpenGL(HWND hwnd, HDC *, HGLRC *);
 void DisableOpenGL(HWND, HDC, HGLRC);
 
 GLuint loadTexture(const char *filename, int *width, int *height);
+
 int imageWidth, imageHeight;
 GLuint textureID;
 
@@ -31,7 +32,9 @@ public:
     float x, y, width, height;
     GLuint textureID;
 
-    Button(float _x, float _y, float _width, float _height, GLuint _textureID) : x(_x), y(_y), width(_width), height(_height), textureID(_textureID) {}
+    Button(float _x, float _y, float _width, float _height, GLuint _textureID) : x(_x), y(_y), width(_width),
+        height(_height), textureID(_textureID) {
+    }
 
     bool isClicked(float clickX, float clickY) {
         return (clickX >= x && clickX <= x + width && clickY >= y && clickY <= y + height);
@@ -53,7 +56,7 @@ public:
 };
 
 Button button1(-100.0f, 0.0f, 100.0f, 50.0f, 0); // Button 1
-Button button2(0.0f, 0.0f, 100.0f, 50.0f, 0);   // Button 2
+Button button2(0.0f, 0.0f, 100.0f, 50.0f, 0); // Button 2
 Button button3(100.0f, 0.0f, 100.0f, 50.0f, 0); // Button 3
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -93,7 +96,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
 
-// NOTE : CONSTRUCTION ZONE
+    // NOTE : CONSTRUCTION ZONE
     // glEnable(GL_DEPTH_TEST); // Enable depth testing
     // glDisable(GL_BLEND);
 
@@ -115,7 +118,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
             std::cout << "Clearing screen..." << std::endl;
             std::cout << "Rendering texture..." << std::endl;
+
+            // Reset color to default (white) before rendering the texture
+            // glColor3f(0.0f, 1.0f, 1.0f);
             renderTexture(textureID); // Render the texture
+
+            // Reset color to default (white) before rendering the texture
+            // glColor3f(1.0f, 1.0f, 1.0f);
+            renderRectangle();
 
             // button1.render(); // Render button 1
             // button2.render(); // Render button 2
@@ -132,9 +142,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
 
-
-
-// =============
+    // =============
 
     DisableOpenGL(hwnd, hDC, hRC);
     DestroyWindow(hwnd);
@@ -146,7 +154,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     switch (uMsg) {
         case WM_CLOSE:
             PostQuitMessage(0);
-        break;
+            break;
 
         case WM_DESTROY:
             return 0;
@@ -155,7 +163,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             if (wParam == VK_ESCAPE) {
                 PostQuitMessage(0);
             }
-        break;
+            break;
 
         default:
             // std::cout << "Key pressed: " + wParam + '\n';
@@ -189,7 +197,6 @@ void EnableOpenGL(HWND hwnd, HDC *hDC, HGLRC *hRC) {
     GetClientRect(hwnd, &rect);
     glViewport(0, 0, rect.right - rect.left, rect.bottom - rect.top); // Adjust viewport size
     std::cout << "Viewport size: " << (rect.right - rect.left) << "x" << (rect.bottom - rect.top) << std::endl;
-
 }
 
 void DisableOpenGL(HWND hwnd, HDC hDC, HGLRC hRC) {
@@ -240,7 +247,12 @@ GLuint loadTexture(const char *filename, int *width, int *height) {
 
 void renderTexture(GLuint textureID) {
     glEnable(GL_TEXTURE_2D);
+
     glBindTexture(GL_TEXTURE_2D, textureID); // Bind the texture
+
+    // glColor3f(1.0f, 1.0f, 1.0f);
+    // Set the texture environment mode to GL_REPLACE
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
     // Check if texture is bound successfully
     GLint boundTexture;
@@ -251,12 +263,20 @@ void renderTexture(GLuint textureID) {
         std::cerr << "Failed to bind texture: " << textureID << std::endl;
     }
 
+
     glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0f, -1.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f(1.0f, -1.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex2f(1.0f, 1.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0f, 1.0f);
+
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex2f(-1.0f, -1.0f);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex2f(1.0f, -1.0f);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex2f(-1.0f, 1.0f);
+
     glEnd();
+
 
     glDisable(GL_TEXTURE_2D);
 
@@ -268,17 +288,16 @@ void renderTexture(GLuint textureID) {
 }
 
 
-
-
-
-
-
 void renderRectangle() {
     glColor3f(1.0f, 0.0f, 0.0f); // Set color to red
+
+
     glBegin(GL_QUADS);
-    glVertex2f(-0.5f, -0.5f); // Bottom-left corner
-    glVertex2f(0.5f, -0.5f); // Bottom-right corner
-    glVertex2f(0.5f, 0.5f); // Top-right corner
-    glVertex2f(-0.5f, 0.5f); // Top-left corner
+
+        glVertex2f(-0.5f, -0.5f); // Bottom-left corner
+        glVertex2f(0.5f, -0.5f); // Bottom-right corner
+        glVertex2f(0.5f, 0.5f); // Top-right corner
+        glVertex2f(-0.5f, 0.5f); // Top-left corner
+
     glEnd();
 }
